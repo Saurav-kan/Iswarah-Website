5180a7e631dd894182794805bcacd86637d5969e
Saurav Kandel <sauravk0633@gmail.com>
2025-05-22 19:58:24 -0500

    Day2

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-created_at')
+    serializer_class = ProductSerializer
